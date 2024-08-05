8834979634d706c8fa5cf6116e1fb07aaf037213
ki-wing <dldpsk151@gmail.com>
2024-08-05 09:05:22 +0900

    feat: Add vue basic

---
+++ order-frontend-main/src/components/TestComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>this is Testcomponent</h1>
+</template>
+
+<script>
+export default{};
+</script>
---
+++ order-frontend-main/src/main.js
@@ -0,0 +1,15 @@
+// main.js는 vue애플리케이션의 시작점
+
+import { createApp } from 'vue'
+import App from './App.vue'
+// src/router/index.js 파일의 router를 사용하겠다는 선언
+import router from '@/router/index.js'
+import vuetify from './plugins/vuetify';
+// createApp(App).mount('#app')
+const app = createApp(App);
+
+
+
+app.use(router)
+app.use(vuetify)
+app.mount('#app');
