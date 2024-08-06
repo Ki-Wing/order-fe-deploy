8194187383bbb6b5b74f4d3eda7eda04b6341157
ki-wing <dldpsk151@gmail.com>
2024-08-06 15:51:29 +0900

    style: Add product vue

--- order-frontend-main/src/components/ProductListComponent.vue
+++ order-frontend-main/src/components/ProductListComponent.vue
@@ -1,5 +1,25 @@
 <template>
-    
+    <v-container>
+        <v-row class="d-flex justify-content-between mt-5">
+            <v-col>
+                <!-- <v-form @submit.prevent="searchProducts"> -->
+                <v-form>
+                    
+                </v-form>
+            </v-col>
+
+            <v-col cols="auto">
+                <v-btn>장바구니</v-btn>
+                <v-btn>주문하기</v-btn>
+            </v-col>
+            <v-col cols="auto">
+
+            <!-- <v-col cols="auto" v-if="isAdmin"> -->
+                <v-btn href="/product/create" color="success">상품등록</v-btn>
+                
+            </v-col>
+        </v-row>
+    </v-container>
 
 
 </template>
@@ -7,6 +27,9 @@
 <script>
 export default {
     data() {
+        return{
+
+        }
         
     },
 }
