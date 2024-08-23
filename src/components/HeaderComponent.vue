07e6e11da153f77513dfd65df6b1cb3bfe08dd15
ki-wing <dldpsk151@gmail.com>
2024-08-23 12:26:38 +0900

    feat: CloudFront test

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -10,7 +10,7 @@
                          </div>
                     </v-col>
                 <v-col class="text-center">
-                    <v-btn :to="{path:'/'}">jjapmu shop</v-btn>
+                    <v-btn :to="{path:'/'}">JJAPMU STORE</v-btn>
                 </v-col>
                 <v-col class="d-flex justify-end">
                     <v-btn v-if="isLogin"  :to="{path:'/order/cart'}">장바구니({{ getTotalQuantity }})</v-btn>
